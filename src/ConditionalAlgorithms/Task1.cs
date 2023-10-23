f713e03a2c085e9d4872634b63bdff93eb635532
Mihail.P <144612697+KP-31-Pshenychka-Mykhailo@users.noreply.github.com>
2023-10-23 20:17:03 +0300

    added code for 1 task

--- src/ConditionalAlgorithms/Task1.cs
+++ src/ConditionalAlgorithms/Task1.cs
@@ -1,9 +1,47 @@
 ﻿namespace ConditionalAlgorithms;
-
-public class Task1
 {
-    public (double x, double y) Calculate(double a, double b, double c)
+    internal class Program
     {
-        throw new NotImplementedException();
+
+        public static void Main(string[] args)
+        {
+             Console.InputEncoding = Encoding.Unicode;
+             Console.OutputEncoding = Encoding.Unicode;
+            
+            Console.WriteLine("Введіть a, b і c:");
+
+            double x = Convert.ToDouble(Console.ReadLine());
+            double y = Convert.ToDouble(Console.ReadLine());
+            double z = Convert.ToDouble(Console.ReadLine());
+            if (z == 0)
+            {
+                Console.WriteLine("Помилка");
+            }
+            else
+            {
+                Task1 task = new Task1();
+                (double a, double b) result = task.Calculate(x, y, z);
+
+                Console.WriteLine($"x = {result.a}");
+                Console.WriteLine($"y = {result.b}");
+            }
+        }
+
+        public class Task1
+        {
+            public (double a, double b) Calculate(double x, double y, double z)
+            {
+                double a, b;
+                
+                
+                 a = Math.Cos(x + ((x * y) / z));
+
+                 b = (x * x * x) / Math.Cos(a);
+
+                return (a, b);
+            }
+        }
+     
+
     }
 }
